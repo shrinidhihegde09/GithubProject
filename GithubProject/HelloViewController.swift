721b8b88d5f5a0d52e25a18548179425a32319dd
shrinidhihegde09 <shrinidhi.hegde@capgemini.com>
2022-09-19 03:11:53 -0700

    Add HelloViewController

---
+++ GithubProject/HelloViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HelloViewController.swift
+//  GithubProject
+//
+//  Created by Capgemini-DA311 on 9/19/22.
+//
+
+import UIKit
+
+class HelloViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    override func printContent(_ sender: Any?) {
+        print("Hello")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
