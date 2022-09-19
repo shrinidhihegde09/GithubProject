0a13c98c89595035c754569cf5632a6b6be63e83
shrinidhihegde09 <shrinidhi.hegde@capgemini.com>
2022-09-19 02:59:56 -0700

    Getting started with GitHub

--- GithubProject/ViewController.swift
+++ GithubProject/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func printContent(_ sender: Any?) {
+        print("Hello, Welcome to GitHub")
+    }
 
 
 }
